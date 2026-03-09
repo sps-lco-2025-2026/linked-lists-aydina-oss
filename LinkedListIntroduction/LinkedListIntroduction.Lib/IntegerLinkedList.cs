89ff48b491069f57e832c4ee3dbe3c530ba43d44
aydina-oss <aydina@stpaulsschool.org.uk>
2026-03-09 11:19:09 +0000

    aa

--- LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
+++ LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
@@ -1,64 +1,69 @@
-﻿namespace LinkedListIntroduction.Lib;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class IntegerLinkedList
+namespace LinkedListExample.Lib
 {
-    IntegerNode _head;
-   
-    public IntegerLinkedList()
+    public class IntegerLinkedList
     {
-        _head = null;
-    }
+        IntegerNode _head;
 
-    public IntegerLinkedList(int v)
-    {
-        _head = new IntegerNode(v);
-    }
+        public IntegerLinkedList()
+        {
+            _head = null;
+        }
 
-    public int Count => _head == null ? 0 : _head.Count;
-    public int Sum => _head == null ? 0 : _head.Sum;
-
-    public void Append(int v)
-    {
-        if (_head == null)
+        public IntegerLinkedList(int v)
+        {
             _head = new IntegerNode(v);
-        else
-            _head.Append(v);
+        }
 
+        public int Count => _head == null ? 0 : _head.Count;
+        public int Sum => _head == null ? 0 : _head.Sum;
+
+        public void Append(int v)
+        {
+            if (_head == null)
+                _head = new IntegerNode(v);
+            else
+                _head.Append(v);
+
+        }
+
+        public override string ToString()
+        {
+            return _head == null ? "{}" : $"{{{_head}}}";
+        }
     }
-
-    public override string ToString()
+    class IntegerNode
     {
-        return _head == null ? "{}" : $"{{{_head}}}";
-    }
-}
+        int _value;
+        IntegerNode _next;
 
-public class IntegerNode
-{
-    int _value;
-    IntegerNode _next;
-
-     internal int Count => _next == null ? 1 : 1 + _next.Count;
+        internal int Count => _next == null ? 1 : 1 + _next.Count;
             
-    internal int Sum => _next == null ? _value : _value + _next.Sum;
+        internal int Sum => _next == null ? _value : _value + _next.Sum;
 
 
-    internal IntegerNode(int v)
-    {
-        _value = v;
-        _next = null;
+        internal IntegerNode(int v)
+        {
+            _value = v;
+            _next = null;
+        }
+
+        internal void Append(int v)
+        {
+            if (_next == null)
+                _next = new IntegerNode(v);
+            else
+                _next.Append(v);
+        }
+
+        public override string ToString()
+        {
+            return _next == null ? _value.ToString() : $"{_value}, {_next}";
+        }
     }
-
-    internal void Append(int v)
-    {
-        if (_next == null)
-            _next = new IntegerNode(v);
-        else
-            _next.Append(v);
-    }
-
-    public override string ToString()
-    {
-        return _next == null ? _value.ToString() : $"{_value}, {_next}";
-    }
-
 }
